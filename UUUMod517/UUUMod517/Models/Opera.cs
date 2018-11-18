90ca33ac226904531a68d80f3d1829c8f032be60
parnpahn <parnpahn@gmail.com>
2018-11-18 11:47:04 +0800

    增加Opera class

---
+++ UUUMod517/UUUMod517/Models/Opera.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UUUMod517.Models
+{
+    public class Opera
+    {
+        public int OperaID { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Composer { get; set; }
+    }
+}
