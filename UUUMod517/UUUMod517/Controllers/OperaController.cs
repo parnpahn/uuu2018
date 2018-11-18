d34924d924ce6ed454bd417826af7a449248d14c
parnpahn <parnpahn@gmail.com>
2018-11-18 11:55:42 +0800

    新增Opera 控制項  並將view model 傳向檢視

---
+++ UUUMod517/UUUMod517/Controllers/OperaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UUUMod517.Models;
+
+namespace UUUMod517.Controllers
+{
+    public class OperaController : Controller
+    {
+        // GET: Opera
+        public ActionResult Index()
+        {
+            Opera o = new Opera()
+            {
+                OperaID=1,Title="胖胖胖胖",Year=107,Composer="胖胖胖胖胖胖胖胖"
+            }
+            return View();
+        }
+    }
+}
